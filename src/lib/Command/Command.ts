2469fa2bf5a529f2d90d0a496586f7d0c509a088
akuny <andykuny@gmail.com>
2020-08-25 13:06:21 -0400

    Refactor Command isValid() class

--- src/lib/Command/Command.ts
+++ src/lib/Command/Command.ts
@@ -1,4 +1,10 @@
 import { iCommand, iCommandPayload } from '../../ts/interfaces';
+import {
+    validMovementCommands,
+    validUseCommands,
+    validTakeCommands,
+    validUtilityCommands,
+} from './commands';
 
 export class Command implements iCommand {
     private valid: boolean;
@@ -9,32 +15,21 @@ export class Command implements iCommand {
     private validUseCommands: string[];
     private validTakeCommands: string[];
     private validUtilityCommands: string[];
+    private allCommands: string[];
 
     constructor(rawInput: string) {
         this.valid = false;
         this.type = '';
         this.action = '';
         this.items = ['none'];
-        this.validMovementCommands = [
-            'n',
-            'north',
-            's',
-            'south',
-            'e',
-            'east',
-            'w',
-            'west',
-        ];
-        this.validUseCommands = ['use', 'play', 'open'];
-        this.validTakeCommands = ['take'];
-        this.validUtilityCommands = [
-            'h',
-            'help',
-            'l',
-            'look',
-            'i',
-            'inventory',
-        ];
+        this.validMovementCommands = validMovementCommands;
+        this.validUseCommands = validUseCommands;
+        this.validTakeCommands = validTakeCommands;
+        this.validUtilityCommands = validUtilityCommands;
+        this.allCommands = this.validMovementCommands
+            .concat(this.validTakeCommands)
+            .concat(this.validUseCommands)
+            .concat(this.validUtilityCommands);
         this.validate(rawInput);
     }
 
@@ -51,77 +46,85 @@ export class Command implements iCommand {
     }
 
     private validate(rawInput: string) {
-        const trimmedInput = rawInput.toLowerCase().trim();
-        const splitInput = trimmedInput.split(' ');
+        const splitInput = rawInput.toLowerCase().trim().split(' ');
 
-        for (let i = 0; i < splitInput.length; i++) {
-            const word = splitInput[i];
-            if (this.validMovementCommands.includes(word)) {
-                this.valid = true;
-                this.action = word;
-
-                switch (this.action) {
-                    case 'n':
-                        this.action = 'north';
-                        break;
-                    case 's':
-                        this.action = 'south';
-                        break;
-                    case 'e':
-                        this.action = 'east';
-                        break;
-                    case 'w':
-                        this.action = 'west';
-                        break;
-                    default:
-                        break;
-                }
-
-                this.type = 'move';
-            }
-
-            if (this.validUseCommands.includes(word)) {
-                this.valid = true;
-                this.action = word;
-                this.type = 'use';
-            }
-
-            if (this.validTakeCommands.includes(word)) {
-                this.valid = true;
-                this.action = word;
-                this.type = 'take';
-            }
-
-            if (this.validUtilityCommands.includes(word)) {
-                this.valid = true;
-                this.action = word;
-                switch (this.action) {
-                    case 'h':
-                        this.action = 'help';
-                        break;
-                    case 'l':
-                        this.action = 'look';
-                        break;
-                    case 'iprivate items: string[];':
-                        this.action = 'inventory';
-                        break;
-                    default:
-                        break;
-                }
-                this.type = 'other';
-            }
-
-            const allCommands = this.validMovementCommands
-                .concat(this.validTakeCommands)
-                .concat(this.validUseCommands)
-                .concat(this.validUtilityCommands);
-
-            if (!allCommands.includes(word)) {
+        splitInput.forEach((word) => {
+            if (!this.allCommands.includes(word)) {
                 this.items = this.items.filter((str) => {
                     return str !== 'none';
                 });
                 this.items.push(word);
             }
+
+            if (this.validMovementCommands.includes(word)) {
+                this.handleMovementCommand(word);
+            }
+
+            if (this.validUseCommands.includes(word)) {
+                this.handleUseCommand(word);
+            }
+
+            if (this.validTakeCommands.includes(word)) {
+                this.handleTakeCommand(word);
+            }
+
+            if (this.validUtilityCommands.includes(word)) {
+                this.handleUtilityCommand(word);
+            }
+        });
+    }
+
+    private handleMovementCommand(word: string) {
+        this.valid = true;
+        this.action = word;
+
+        switch (this.action) {
+            case 'n':
+                this.action = 'north';
+                break;
+            case 's':
+                this.action = 'south';
+                break;
+            case 'e':
+                this.action = 'east';
+                break;
+            case 'w':
+                this.action = 'west';
+                break;
+            default:
+                break;
         }
+        this.type = 'move';
+    }
+
+    private handleUseCommand(word: string) {
+        this.valid = true;
+        this.action = word;
+        this.type = 'use';
+    }
+
+    private handleTakeCommand(word: string) {
+        this.valid = true;
+        this.action = word;
+        this.type = 'take';
+    }
+
+    private handleUtilityCommand(word: string) {
+        this.valid = true;
+        this.action = word;
+        switch (this.action) {
+            case 'h':
+                this.action = 'help';
+                break;
+            case 'l':
+                this.action = 'look';
+                break;
+            case 'i':
+                this.action = 'inventory';
+                break;
+            default:
+                break;
+        }
+        this.type = 'other';
     }
 }
---
+++ src/lib/Command/commands.ts
@@ -0,0 +1,23 @@
+export const validMovementCommands = [
+    'n',
+    'north',
+    's',
+    'south',
+    'e',
+    'east',
+    'w',
+    'west',
+];
+
+export const validUseCommands = ['use', 'play', 'open'];
+
+export const validTakeCommands = ['take'];
+
+export const validUtilityCommands = [
+    'h',
+    'help',
+    'l',
+    'look',
+    'i',
+    'inventory',
+];
